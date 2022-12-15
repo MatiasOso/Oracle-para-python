8aeb45fd44b475281c79c31035a161eaaeca7974
MatiasOso <matias.osores@inacapmail.cl>
2022-12-15 13:06:38 -0300

    D:

--- ventana.py
+++ ventana.py
@@ -7,25 +7,36 @@ import os
 os.environ['TNS_ADMIN']='C:/Users/Usuario/Desktop/Oracle/wallet' 
 connection=cx_Oracle.connect('ADMIN','TI2002-sec-213','db20220606233104_high')
 cursor=connection.cursor()
-#Cajero
-class InfoProd ():
-    def __init__(self) :
-        self.root = tk.Tk()
-        self.root.title('InfoPROD')
-        self.root.geometry('400x200')
-        self.root.resizable(0, 0)
+#Registro      
+
+class ProdModifier(tk.Tk):
+    def __init__(self,container):
+        super().__init__(container)
+        self.title('Buscador de productos')
+        self.geometry('300x100')
+        self.resizable(0, 0)
         self.__create_widgets()
-        self.root.mainloop()
+    def destroy(self):
+        super().destroy()  
     def __create_widgets(self):
-        ttk.Label(self.root,text='CodProd').grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='Nombre').grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='PrecioVta').grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='Stock').grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='PrecioCto').grid(column=0, row=4, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='VentasHoy').grid(column=0, row=5, sticky=tk.W, padx=5, pady=5)
-        ttk.Label(self.root,text='Venta ultimos 30 dias').grid(column=0, row=6, sticky=tk.W, padx=5, pady=5)
-        
+        CodProd=ttk.Label(self.root,text='CodProd').grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        Nombre=ttk.Label(self.root,text='Nombre').grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        PrecioVta=ttk.Label(self.root,text='PrecioVta').grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+        Stock=ttk.Label(self.root,text='Stock').grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+        PrecioCto=ttk.Label(self.root,text='PrecioCto').grid(column=0, row=4, sticky=tk.W, padx=5, pady=5)
+        self.cajaCodProd = ttk.Entry(self.root, width=30)
+        self.cajaCodProd.grid(column=1, row=0, sticky=tk.W)
+        self.cajaNombre = ttk.Entry(self.root, width=30)
+        self.cajaNombre.grid(column=1, row=1, sticky=tk.W)
+        self.cajaPrecioVta = ttk.Entry(self.root, width=30)
+        self.cajaPrecioVta.grid(column=1, row=2, sticky=tk.W)
+        self.cajaStock = ttk.Entry(self.root, width=30)
+        self.cajaStock.grid(column=1, row=3, sticky=tk.W)
+        self.cajaPrecioCto = ttk.Entry(self.root, width=30)
+        self.cajaPrecioCto.grid(column=1, row=4, sticky=tk.W)
+        ttk.Button(self.root, text='Salir',command=lambda: self.destroy()).grid(column=2, row=4)
+
 
 if __name__ == "__main__":
-    app = InfoProd()
+    app =ProdModifier(container=None)
     app.mainloop()
